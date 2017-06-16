a4c503ff51c26bac05a27c1c9e3dea0ecd4f09b8
MaryGao <Gao0o0@outlook.com>
2017-06-16 01:45:10 -0700

    updete sql 01
    updete sql 01

--- sql.sql
+++ sql.sql
@@ -1,32 +1,38 @@
 CREATE database stu ;
 use stu; 
 CREATE TABLE school(
-    pno char(20) PRIMARY KEY,
-    pname char(40) UNIQUE NOT NULL, 
-    dname char(40) UNIQUE NOT NULL
+    pno char(5) PRIMARY KEY,
+    pname char(30) UNIQUE NOT NULL, 
+    dname char(30) UNIQUE NOT NULL
 );
 CREATE TABLE information ( 
-    sno char(20) PRIMARY KEY,
-    sname char(20) NOT NULL,
+    sno char(12) PRIMARY KEY,
+    id char(18) UNIQUE NOT NULL,
+    sname char(10) NOT NULL,
     sex CHAR(2) CHECK (sex IN('男','女')),
-    sage SMALLINT CHECK(sage>10 and sage<=60),
+    birth date NOT NULL,
     tel CHAR(11) UNIQUE NOT NULL,                                                          
-    adress char(30) UNIQUE NOT NULL,
-    dno char(20),
-    FOREIGN KEY (dno) REFERENCES school(pno)
+    adress varchar(50) NOT NULL,
+    pno char(5),
+    FOREIGN KEY (pno) REFERENCES school(pno)
 );
 CREATE TABLE course(
-    cno char(10) PRIMARY KEY,
+    cno char(6) PRIMARY KEY,
     cname char(20) UNIQUE NOT NULL,
-    cpan char(10),
+    cpan char(6),
     ceredit SMALLINT CHECK(ceredit>0 and ceredit<10) ,
     FOREIGN KEY(cpan) REFERENCES course(cno)
 );
 CREATE TABLE score(
-    sno char(20),
-    cno char(11),
+    sno char(12),
+    cno char(5),
     grade SMALLINT CHECK(grade >=0 and grade <=100),
     PRIMARY KEY (sno,cno),
     FOREIGN KEY(sno) REFERENCES information(sno),
     FOREIGN KEY(cno) REFERENCES course(cno)
+);
+CREATE TABLE teacher(
+    tno char(8) PRIMARY KEY,
+    tname char(10),
+    salary SMALLINT CHECK(salary >0)
 );
