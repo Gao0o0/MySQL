b6cc0ab28eeda066ffdd3baba85154c85dc77762
Richard.Wang <wangleihd@gmail.com>
2017-06-20 18:01:00 +0800

    add YUANma
    add YUANma

--- sql.sql
+++ sql.sql
@@ -3,8 +3,10 @@ use stu;
 CREATE TABLE school(
     pno char(5) PRIMARY KEY,
     pname char(30) UNIQUE NOT NULL, 
-    dname char(30) NOT NULL
+    dname char(30) NOT NULL,
+    state char(1) DEFAULT 1 CHECK(state in ('0','1'))
 )character set utf8;
+
 CREATE TABLE information ( 
     sno char(12) PRIMARY KEY,
     sname char(10) NOT NULL,
@@ -13,23 +15,30 @@ CREATE TABLE information (
     tel CHAR(11) UNIQUE NOT NULL,                                                          
     adress varchar(50) NOT NULL,
     pno char(5),
+    state char(1) DEFAULT 1 CHECK(state in ('0','1')),
     FOREIGN KEY (pno) REFERENCES school(pno)
 )character set utf8;
+
 CREATE TABLE course(
     cno char(6) PRIMARY KEY,
     cname char(20) UNIQUE NOT NULL,
     cpan char(6),
     ceredit SMALLINT CHECK(ceredit>0 and ceredit<10) ,
+    num SMALLINT DEFAULT 0 CHECK(num >=0 and num<=50 ) ,
+    state char(1) DEFAULT 1 CHECK(state in ('0','1')),
     FOREIGN KEY(cpan) REFERENCES course(cno)
 )character set utf8;
+/*alter table course add num SMALLINT DEFAULT 0 CHECK(num >=0 and num<=50 );*/
+/*alter table score add state char(1) DEFAULT 1 CHECK(state in ('0','1'));*/
 CREATE TABLE score(
     sno char(12),
     cno char(6),
     grade SMALLINT CHECK(grade >=0 and grade <=100),
     PRIMARY KEY (sno,cno),
     FOREIGN KEY(sno) REFERENCES information(sno),
+    state char(1) DEFAULT 1 CHECK(state in ('0','1')),
     FOREIGN KEY(cno) REFERENCES course(cno)
-);
+)character set utf8;
 CREATE TABLE teacher(
     tno char(8) PRIMARY KEY,
     tname char(10),
@@ -60,3 +69,11 @@ insert into score
 values ('414109070128','120001',89);
 insert into score
 values ('414109070128','120101',null);
+
+create table lod(
+uid char(12) primary key,
+perm int check (perm in(0,1)),
+pwd char(6) default '000000'
+);
+insert into lod
+values('111111111111',0,'123456');
